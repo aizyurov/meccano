e8f0267808e3fa51eb03253ea0a866c7ff273c03
lvovich <lvovich@yandex-team.ru>
2015-11-13 19:51:39 +0300

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@
 use std::collections::HashMap;
 use std::cell::RefCell;
 use std::any::Any;
+// use std::cell::BorrowState;
+
 
 extern crate anymap;
 
@@ -32,13 +34,13 @@ enum CachedObject<T> {
 
 struct Binding<T> {
 	ctr: Box<Constructor<T>>,
-	obj: CachedObject<T>,
+	obj: Option<T>,
 }
 
 impl <T: Any + Clone> Binding<T> {
 
 	fn new(constructor: Box<Constructor<T>>) -> Binding<T> {
-		Binding{ctr: constructor, obj: CachedObject::New}
+		Binding{ctr: constructor, obj: Option::None}
 	}
 	
 }
@@ -50,32 +52,40 @@ impl Context {
 	}
 
 	pub fn named<T: Any + Clone>(&self, name: &str) -> T {
-		let inner_map = self.map.get::<HashMap<String, RefCell<Binding<T>>>>().expect("Unresolved context object");
-		let binding_ref = inner_map.get(name).expect("Unresolved context object");
-		let cached: Option<T>;
+		let inner_map = self.map.get::<HashMap<String, RefCell<Binding<T>>>>().expect("Unresolved dependency type");
+		let binding_ref = inner_map.get(name).expect(&format!("Unresolved dependency label {}", &name));
+		let cached_value: Option<T>;
+		// This staff waits for 1.5. For the time being we are getting "Already borrowed" instead of
+		// "Cyclic dependency"
+//		match binding_ref.borrow_state() {
+//			BorrowState::Unused => {
+//				let mut binding = binding_ref.borrow();
+//				match binding.obj {
+//					Option::Some(ref value) => cached_value = Option::Some(value.clone()),
+//					Option::None => { cached_value = Option::None }
+//				}
+//			}
+//			// if already borrowed, the same object is requested while it is under constuction
+//			_ => panic!("Cyclic dependency")
+//		}
+
 		{
 			let binding = binding_ref.borrow();
-			cached = match binding.obj {
-				CachedObject::New => Option::None,
-				CachedObject::Ready(ref ready) => Option::Some(ready.clone()), 
-				CachedObject::UnderConstruction => panic!("Cyclic dependency in context"),
+			match binding.obj {
+				Some(ref value) => cached_value = Option::Some(value.clone()),
+				None =>  cached_value = Option::None
 			}
 		}
-		// borrow ends here
-		match cached {
-			Some(x) => x,
-			None => {
-				// it is safe to borrow mutable reference
+		match cached_value {
+			Some(value) => value,
+			None => {	
 				let mut binding = binding_ref.borrow_mut();
-				// protect fron duplicate borrow_nut
-				binding.obj = CachedObject::UnderConstruction;
-				let t = binding.ctr.construct(self); 
-				binding.obj = CachedObject::Ready(t.clone());
+				let t = binding.ctr.construct(self);
+				binding.obj = Some(t.clone());
 				t
 			}
-		}		
-	}
-	
+		}
+	}	
 }
 
 pub struct Builder {
@@ -88,7 +98,7 @@ impl Builder {
 		Builder{map: AnyMap::new()}
 	}
 
-	pub fn label<T: Any + Clone + Send, C>(&mut self, name: &str, ctr: C)
+	pub fn label<T: Any + Clone, C>(&mut self, name: &str, ctr: C)
 		where C: Constructor<T>		
 	{
 		if !self.map.contains::<HashMap<String, RefCell<Binding<T>>>>() {
@@ -98,7 +108,7 @@ impl Builder {
 		inner_map.insert(name.to_string(), RefCell::new(Binding::new(Box::new(ctr))));
 	}
 
-	pub fn add<T: Any + Clone + Sync + Send, C>(&mut self, ctr: C)
+	pub fn add<T: Any + Clone, C>(&mut self, ctr: C)
 		where C: Constructor<T>		
 	{
 		self.label("", ctr);
@@ -119,7 +129,7 @@ fn it_works() {
 	builder.add(|ctx: & Context| {32});
 	builder.add(|ctx: & Context| {33});
 	let context = builder.build();
-	println!("got from context {}", context.get::<i32>());
+	assert_eq!(context.get::<i32>(), 33);
 }
 
 
